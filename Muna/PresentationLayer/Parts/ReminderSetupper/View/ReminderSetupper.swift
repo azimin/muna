77fc6c4a4752f6d380f77efd18c9612d0dac5f04
Egor Petrov <egor@barbatosso.com>
2020-05-09 03:22:45 +0300

    First steps of reminder setupper

--- Muna/PresentationLayer/Parts/ReminderSetupper/View/ReminderSetupper.swift
+++ Muna/PresentationLayer/Parts/ReminderSetupper/View/ReminderSetupper.swift
@@ -8,10 +8,34 @@
 
 import Cocoa
 
+protocol ReminderSetupperDelegate: AnyObject {
+    func doneButtonTapped()
+}
+
 class ReminderSetupper: NSView {
+    weak var delegate: ReminderSetupperDelegate?
 
     let dateTextField = TextField(fontStyle: .medium, size: 14)
     let commentTextField = TextField(fontStyle: .medium, size: 14)
 
-    
+    let button = Button(
+        title: "Done",
+        target: self,
+        action: #selector(handleDoneButtonTap)
+    )
+
+    init(delegate: ReminderSetupperDelegate) {
+        self.delegate = delegate
+
+        super.init(frame: .zero)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc
+    func handleDoneButtonTap() {
+        self.delegate?.doneButtonTapped()
+    }
 }
