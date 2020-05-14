5f5fd3d4d83a240ea7f00b82485cdbcf2321b6fc
Alexander <azimin@me.com>
2020-05-14 22:51:56 +0300

    Setup settings basics

---
+++ Muna/PresentationLayer/Parts/Settings/General/View/SettingsView.swift
@@ -0,0 +1,11 @@
+//
+//  SettingsView.swift
+//  Muna
+//
+//  Created by Alexander on 5/14/20.
+//  Copyright © 2020 Abstract. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsView: View {}
