b6b94c05bdc3fe7a517cf58a175a90d1481fbbc6
Alexander <azimin@me.com>
2020-05-24 18:01:34 +0300

    Refactored task view

---
+++ Muna/PresentationLayer/UI/Components/DateParser/DateParserView.swift
@@ -0,0 +1,165 @@
+//
+//  DateParserView.swift
+//  Muna
+//
+//  Created by Alexander on 5/24/20.
+//  Copyright © 2020 Abstract. All rights reserved.
+//
+
+import Cocoa
+
+class DateParserView: View, RemindersOptionsControllerDelegate {
+    var mainOption: TaskReminderItemView?
+    var options: [TaskReminderItemView] = []
+    let itemsStackView = NSStackView(
+        orientation: .vertical,
+        distribution: .fill
+    )
+
+    var selectedIndex: Int = 0
+
+    var controller: RemindersOptionsController
+
+    init(controller: RemindersOptionsController) {
+        self.controller = controller
+        super.init(frame: .zero)
+        self.setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setup() {
+        self.addSubview(self.itemsStackView)
+        self.itemsStackView.snp.makeConstraints { maker in
+            maker.edges.equalToSuperview()
+        }
+
+        let option = TaskReminderItemView()
+        self.mainOption = option
+        self.mainOption?.infoLabel.text = ""
+
+        self.itemsStackView.addArrangedSubview(option)
+        self.itemsStackView.setCustomSpacing(6, after: option)
+
+        option.update(style: .basic)
+        self.options.append(option)
+
+        self.controller.delegate = self
+    }
+
+    // MARK: - RemindersOptionsControllerDelegate
+
+    func remindersOptionsControllerShowItems(
+        _ controller: RemindersOptionsController,
+        items: [RemindersOptionsController.ReminderItem]
+    ) {
+        self.mainOption = self.options.first
+        self.shouldRunCompletion = false
+
+        var subviewIndex: Int
+        if let option = self.mainOption,
+            let index = self.itemsStackView.arrangedSubviews.firstIndex(of: option) {
+            subviewIndex = index
+        } else {
+            assertionFailure("No index")
+            subviewIndex = 0
+        }
+
+        for (index, item) in items.enumerated() where index != 0 {
+            subviewIndex += 1
+            let option: TaskReminderItemView
+            if self.options.count > index {
+                option = self.options[index]
+            } else {
+                option = TaskReminderItemView()
+            }
+            option.update(style: .notSelected, animated: true)
+            option.update(item: item)
+            option.isHidden = true
+            self.itemsStackView.insertArrangedSubview(option, at: subviewIndex)
+            if self.options.count <= index {
+                self.options.append(option)
+            }
+        }
+
+        if items.count == 0 {
+            self.mainOption?.update(style: .basic, animated: true)
+            self.mainOption?.update(item: .init(
+                title: "No reminder",
+                subtitle: "",
+                additionalText: ""
+            ))
+        } else {
+            self.mainOption?.update(style: .selected, animated: true)
+            self.mainOption?.update(item: items[0])
+        }
+
+        let numberOfItems = max(items.count, 1)
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = 0.25
+            context.allowsImplicitAnimation = true
+
+            for optionIndex in 0 ..< numberOfItems {
+                self.options[optionIndex].isHidden = false
+            }
+            if numberOfItems < self.options.count {
+                var offset = 0
+                for itemIndex in numberOfItems ..< self.options.count {
+                    self.options[itemIndex - offset].removeFromSuperview()
+                    self.options.remove(at: itemIndex - offset)
+                    offset += 1
+                }
+            }
+
+            self.layoutSubtreeIfNeeded()
+        }, completionHandler: nil)
+    }
+
+    func remindersOptionsControllerHighliteItem(
+        _ controller: RemindersOptionsController,
+        index: Int
+    ) {
+        for (optionIndex, option) in self.options.enumerated() {
+            option.update(
+                style: optionIndex == index ? .selected : .notSelected
+            )
+        }
+    }
+
+    private var shouldRunCompletion = false
+
+    func remindersOptionsControllerSelectItem(
+        _ controller: RemindersOptionsController,
+        index: Int
+    ) {
+        let option = self.options[index]
+        option.update(style: .basic, animated: true)
+
+        self.shouldRunCompletion = true
+
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = 0.25
+            context.allowsImplicitAnimation = true
+
+            for optionIndex in 0 ..< self.options.count where optionIndex != index {
+                self.options[optionIndex].isHidden = true
+            }
+
+            self.mainOption = option
+            self.layoutSubtreeIfNeeded()
+        }, completionHandler: {
+            guard self.shouldRunCompletion else {
+                return
+            }
+
+            var offset = 0
+            for optionIndex in 0 ..< self.options.count where optionIndex != index {
+                self.options[optionIndex - offset].removeFromSuperview()
+                self.options.remove(at: optionIndex - offset)
+                offset += 1
+            }
+        })
+    }
+}
--- Muna/PresentationLayer/UI/Components/TaskCreate/TaskCreateView.swift
+++ Muna/PresentationLayer/UI/Components/TaskCreate/TaskCreateView.swift
@@ -13,21 +13,15 @@ protocol TaskCreateViewDelegate: AnyObject {
     func shortcutsButtonTapped()
 }
 
-class TaskCreateView: PopupView, RemindersOptionsControllerDelegate {
-    var mainOption: TaskReminderItemView?
-
+class TaskCreateView: PopupView {
     var parsedDates = [DateItem]()
     let presentationDateItemTransformer: DateItemsTransformer
 
-    var options: [TaskReminderItemView] = []
-
-    let contentStackView = NSStackView()
     let doneButton = TaskDoneButton()
 
     let reminderTextField = TextField(clearable: true)
+    let datePrarserView: DateParserView
     let commentTextField = TextField(clearable: false)
-
-    var selectedIndex: Int = 0
 
     var controller = RemindersOptionsController()
 
@@ -37,6 +31,7 @@ class TaskCreateView: PopupView, RemindersOptionsControllerDelegate {
     weak var delegate: TaskCreateViewDelegate?
 
     init(savingProcessingService: SavingProcessingService) {
+        self.datePrarserView = DateParserView(controller: self.controller)
         self.savingProcessingService = savingProcessingService
         self.presentationDateItemTransformer = DateItemsTransformer(dateItems: [], configurator: BasicDateItemPresentationConfigurator())
 
@@ -54,42 +49,35 @@ class TaskCreateView: PopupView, RemindersOptionsControllerDelegate {
             maker.width.equalTo(264)
         }
 
-        self.addSubview(self.contentStackView)
-        self.contentStackView.distribution = .fill
-        self.contentStackView.orientation = .vertical
-        self.contentStackView.snp.makeConstraints { maker in
-            maker.leading.trailing.equalToSuperview().inset(NSEdgeInsets(
-                top: 16,
-                left: 12,
-                bottom: 16,
-                right: 12
-            ))
+        let insets = NSEdgeInsets(top: 16, left: 12, bottom: 16, right: 12)
+
+        self.addSubview(self.reminderTextField)
+        self.reminderTextField.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview().inset(insets)
             maker.top.equalTo(self.closeButton.snp.bottom).inset(-16)
         }
 
-        self.reminderTextField.placeholder = "When to remind"
+        self.addSubview(self.datePrarserView)
+        self.datePrarserView.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview().inset(insets)
+            maker.top.equalTo(self.reminderTextField.snp.bottom).inset(-6)
+        }
 
+        self.addSubview(self.commentTextField)
+        self.commentTextField.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview().inset(insets)
+            maker.top.equalTo(self.datePrarserView.snp.bottom).inset(-6)
+        }
+
+        self.reminderTextField.placeholder = "When to remind"
         self.commentTextField.placeholder = "Comment"
 
-        let option = TaskReminderItemView()
-        self.mainOption = option
-        self.mainOption?.infoLabel.text = ""
-
-        self.contentStackView.addArrangedSubview(self.reminderTextField)
-        self.contentStackView.setCustomSpacing(6, after: self.reminderTextField)
-        self.contentStackView.addArrangedSubview(option)
-        self.contentStackView.setCustomSpacing(6, after: option)
-        self.contentStackView.addArrangedSubview(self.commentTextField)
-
         self.reminderTextField.delegate = self
-
-        option.update(style: .basic)
-        self.options.append(option)
 
         self.addSubview(self.doneButton)
         self.doneButton.snp.makeConstraints { maker in
             maker.bottom.leading.trailing.equalToSuperview()
-            maker.top.equalTo(self.contentStackView.snp.bottom).inset(-32)
+            maker.top.equalTo(self.commentTextField.snp.bottom).inset(-32)
         }
 
         self.doneButton.action = #selector(self.handleDoneButton)
@@ -97,8 +85,6 @@ class TaskCreateView: PopupView, RemindersOptionsControllerDelegate {
         self.shortcutsButton.action = #selector(self.handleShortcutsButton)
 
         self.addMonitor()
-
-        self.controller.delegate = self
     }
 
     var downMonitor: Any?
@@ -121,113 +107,6 @@ class TaskCreateView: PopupView, RemindersOptionsControllerDelegate {
             }
 
             return event
-        })
-    }
-
-    // MARK: - RemindersOptionsControllerDelegate
-
-    func remindersOptionsControllerShowItems(
-        _ controller: RemindersOptionsController,
-        items: [RemindersOptionsController.ReminderItem]
-    ) {
-        self.mainOption = self.options.first
-        self.shouldRunCompletion = false
-
-        var subviewIndex = self.contentStackView.arrangedSubviews.firstIndex(of: self.mainOption!) ?? 0
-
-        for (index, item) in items.enumerated() where index != 0 {
-            subviewIndex += 1
-            let option: TaskReminderItemView
-            if self.options.count > index {
-                option = self.options[index]
-            } else {
-                option = TaskReminderItemView()
-            }
-            option.update(style: .notSelected, animated: true)
-            option.update(item: item)
-            option.isHidden = true
-            self.contentStackView.insertArrangedSubview(option, at: subviewIndex)
-            if self.options.count <= index {
-                self.options.append(option)
-            }
-        }
-
-        if items.count == 0 {
-            self.mainOption?.update(style: .basic, animated: true)
-            self.mainOption?.update(item: .init(
-                title: "No reminder",
-                subtitle: "",
-                additionalText: ""
-            ))
-        } else {
-            self.mainOption?.update(style: .selected, animated: true)
-            self.mainOption?.update(item: items[0])
-        }
-
-        let numberOfItems = max(items.count, 1)
-        NSAnimationContext.runAnimationGroup({ context in
-            context.duration = 0.25
-            context.allowsImplicitAnimation = true
-
-            for optionIndex in 0 ..< numberOfItems {
-                self.options[optionIndex].isHidden = false
-            }
-            if numberOfItems < self.options.count {
-                var offset = 0
-                for itemIndex in numberOfItems ..< self.options.count {
-                    self.options[itemIndex - offset].removeFromSuperview()
-                    self.options.remove(at: itemIndex - offset)
-                    offset += 1
-                }
-            }
-
-            self.layoutSubtreeIfNeeded()
-        }, completionHandler: nil)
-    }
-
-    func remindersOptionsControllerHighliteItem(
-        _ controller: RemindersOptionsController,
-        index: Int
-    ) {
-        for (optionIndex, option) in self.options.enumerated() {
-            option.update(
-                style: optionIndex == index ? .selected : .notSelected
-            )
-        }
-    }
-
-    private var shouldRunCompletion = false
-
-    func remindersOptionsControllerSelectItem(
-        _ controller: RemindersOptionsController,
-        index: Int
-    ) {
-        let option = self.options[index]
-        option.update(style: .basic, animated: true)
-
-        self.shouldRunCompletion = true
-
-        NSAnimationContext.runAnimationGroup({ context in
-            context.duration = 0.25
-            context.allowsImplicitAnimation = true
-
-            for optionIndex in 0 ..< self.options.count where optionIndex != index {
-                self.options[optionIndex].isHidden = true
-            }
-
-            self.mainOption = option
-            self.layoutSubtreeIfNeeded()
-        }, completionHandler: {
-            guard self.shouldRunCompletion else {
-                return
-            }
-
-            var offset = 0
-            for optionIndex in 0 ..< self.options.count where optionIndex != index {
-                self.options[optionIndex - offset].removeFromSuperview()
-                self.options.remove(at: optionIndex - offset)
-                offset += 1
-            }
         })
     }
 
@@ -256,13 +135,13 @@ class TaskCreateView: PopupView, RemindersOptionsControllerDelegate {
 
         var itemToSave = SavingProcessingService.ItemToSave()
 
-        guard let reminderItem = self.controller.item(by: self.selectedIndex) else {
+        guard let reminderItem = self.controller.item(by: self.controller.selectedIndex) else {
             self.savingProcessingService.save(withItem: itemToSave)
             return
         }
 
         itemToSave.dueDateString = reminderItem.title
-        itemToSave.date = self.presentationDateItemTransformer.dates[self.selectedIndex]
+        itemToSave.date = self.presentationDateItemTransformer.dates[self.controller.selectedIndex]
         itemToSave.comment = self.commentTextField.textField.stringValue
 
         self.savingProcessingService.save(withItem: itemToSave)
