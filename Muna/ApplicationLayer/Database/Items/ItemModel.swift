13c4eef69e5c077d9ddd98e897d55675b3d4ce7a
Alexander <azimin@me.com>
2020-07-01 01:42:58 +0300

    Fixed some bugs

--- Muna/ApplicationLayer/Database/Items/ItemModel.swift
+++ Muna/ApplicationLayer/Database/Items/ItemModel.swift
@@ -31,7 +31,7 @@ class ItemModel: ItemModelProtocol, Codable {
     var imageName: String
     var creationDate: Date
 
-    var numberOfTimeChanges: Int? = 0
+    var numberOfTimeChanges: Int?
 
     var dueDateString: String?
     var dueDate: Date? {
