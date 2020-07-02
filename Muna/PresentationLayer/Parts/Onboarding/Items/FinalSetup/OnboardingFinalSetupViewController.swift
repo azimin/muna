422fd15bcadad5a9e6b47ea919dd5563b1ea977b
Egor Petrov <egor@barbatosso.com>
2020-07-02 09:46:44 +0300

    Add setting up

--- Muna/PresentationLayer/Parts/Onboarding/Items/FinalSetup/OnboardingFinalSetupViewController.swift
+++ Muna/PresentationLayer/Parts/Onboarding/Items/FinalSetup/OnboardingFinalSetupViewController.swift
@@ -24,6 +24,9 @@ class OnboardingFinalSetupViewController: NSViewController, OnboardingContainerP
 
         self.rootView.startupSettingItem.switcher.target = self
         self.rootView.startupSettingItem.switcher.action = #selector(self.switchStateChanged)
+
+        self.setupPeriodOfStoring()
+        self.setupPingInterval()
     }
 
     @objc func buttonAction(sender: NSButton) {
@@ -33,5 +36,43 @@ class OnboardingFinalSetupViewController: NSViewController, OnboardingContainerP
     @objc
     func switchStateChanged() {
         Preferences.launchOnStartup = self.rootView.startupSettingItem.switcher.state == .on
+    }
+
+    private func setupPeriodOfStoring() {
+        let value = Preferences.periodOfStoring
+
+        switch value {
+        case .day:
+            self.rootView.storageSettingItem.sliderSectionLabel.text = value.rawValue
+            self.rootView.storageSettingItem.slider.doubleValue = 1
+        case .month:
+            self.rootView.storageSettingItem.sliderSectionLabel.text = value.rawValue
+            self.rootView.storageSettingItem.slider.doubleValue = 2
+        case .week:
+            self.rootView.storageSettingItem.sliderSectionLabel.text = value.rawValue
+            self.rootView.storageSettingItem.slider.doubleValue = 3
+        case .year:
+            self.rootView.storageSettingItem.sliderSectionLabel.text = value.rawValue
+            self.rootView.storageSettingItem.slider.doubleValue = 4
+        }
+    }
+
+    private func setupPingInterval() {
+        let value = Preferences.pingInterval
+
+        switch value {
+        case .fiveMins:
+            self.rootView.notificationsSettingItem.sliderSectionLabel.text = value.rawValue
+            self.rootView.notificationsSettingItem.slider.doubleValue = 1
+        case .tenMins:
+            self.rootView.notificationsSettingItem.sliderSectionLabel.text = value.rawValue
+            self.rootView.notificationsSettingItem.slider.doubleValue = 2
+        case .halfAnHour:
+            self.rootView.notificationsSettingItem.sliderSectionLabel.text = value.rawValue
+            self.rootView.notificationsSettingItem.slider.doubleValue = 3
+        case .hour:
+            self.rootView.notificationsSettingItem.sliderSectionLabel.text = value.rawValue
+            self.rootView.notificationsSettingItem.slider.doubleValue = 4
+        }
     }
 }
