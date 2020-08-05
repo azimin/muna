1d3f56e33eb1435e98fc7d8e98bea455ee00325d
Alexander <azimin@me.com>
2020-08-05 19:37:48 +0300

    Removed Carthage

--- Carthage/Checkouts/appcenter-sdk-apple/Sasquatch/Sasquatch/AppCenterProtocol.swift
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Protocol that all ViewControllers interacting with AppCenter should implement.
- */
-@objc protocol AppCenterProtocol : class {
-  var appCenter : AppCenterDelegate! { get set }
-}
