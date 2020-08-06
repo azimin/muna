c7379e0922c82d6ee54105ead89793abe87c70fc
Egor Petrov <egor@barbatosso.com>
2020-08-06 23:32:51 +0300

    Finish settings

--- Muna/PresentationLayer/Parts/Settings/Tabs/General/GeneralSettingsViewController.swift
+++ Muna/PresentationLayer/Parts/Settings/Tabs/General/GeneralSettingsViewController.swift
@@ -8,12 +8,30 @@
 
 import Cocoa
 
-class GeneralSettingsViewController: NSViewController {
+class GeneralSettingsViewController: NSViewController, ViewHolder {
+    typealias ViewType = GeneralSettingsView
+
+    private let settingsItemViewModel: SettingsItemViewModel
+
+    override init(nibName nibNameOrNil: NSNib.Name?, bundle nibBundleOrNil: Bundle?) {
+        self.settingsItemViewModel = SettingsItemViewModel()
+
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func loadView() {
         self.view = GeneralSettingsView()
+        self.settingsItemViewModel.delegate = self.rootView.settingsItemView
+        self.rootView.settingsItemView.delegate = self.settingsItemViewModel
     }
 
     override func viewDidLoad() {
         self.title = "General"
+
+        self.settingsItemViewModel.setup()
     }
 }
