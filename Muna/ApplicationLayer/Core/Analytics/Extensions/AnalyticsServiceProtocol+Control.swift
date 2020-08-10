8fdec02efa4ea2e5ab0c1fe274e18bb8f5e6ef81
Alexander <azimin@me.com>
2020-08-10 22:27:10 +0300

    Added more analytics

---
+++ Muna/ApplicationLayer/Core/Analytics/Extensions/AnalyticsServiceProtocol+Control.swift
@@ -0,0 +1,32 @@
+//
+//  AnalyticsServiceProtocol+Control.swift
+//  Muna
+//
+//  Created by Alexander on 8/10/20.
+//  Copyright © 2020 Abstract. All rights reserved.
+//
+
+import Foundation
+
+enum AnalyticsControl: String, AnalyticsPropertyNameProtocol {
+    case itemPreview
+    case itemCopy
+    case itemEditTime
+    case itemComplete
+    case itemDelete
+    case itemCreate
+    case panelShortcuts
+    case captureShortcuts
+}
+
+extension AnalyticsServiceProtocol {
+    func executeControl(control: AnalyticsControl, byShortcut: Bool) {
+        self.logEvent(
+            name: "Control Executed",
+            properties: [
+                "type": control.propertiesEventName,
+                "by_shortcut": byShortcut
+            ]
+        )
+    }
+}
