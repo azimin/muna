6678211d7e0aefce166619b66c17a1e8fd925724
Egor Petrov <egor@barbatosso.com>
2020-09-14 20:25:28 +0300

    View and vc for hint

---
+++ Muna/PresentationLayer/Parts/HelperHint/View/HintView.swift
@@ -0,0 +1,36 @@
+//
+//  HintView.swift
+//  Muna
+//
+//  Created by Egor Petrov on 14.09.2020.
+//  Copyright © 2020 Abstract. All rights reserved.
+//
+
+import Cocoa
+import SnapKit
+
+final class HintView: View {
+    let partShorcutView = ShortcutView(item: Preferences.DefaultItems.defaultActivationShortcut.item)
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+
+        self.backgroundColor = ColorStyle.backgroundOverlay.color
+
+        self.setupInitialLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupInitialLayout() {
+        self.addSubview(self.partShorcutView)
+        self.partShorcutView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(12)
+            make.bottom.equalToSuperview().inset(12)
+            make.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
---
+++ Muna/PresentationLayer/Parts/HelperHint/ViewController/HintViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HintViewController.swift
+//  Muna
+//
+//  Created by Egor Petrov on 14.09.2020.
+//  Copyright © 2020 Abstract. All rights reserved.
+//
+
+import Cocoa
+
+class HintViewController: NSViewController, ViewHolder {
+
+    typealias ViewType = HintView
+
+    override func loadView() {
+        self.view = HintView()
+    }
+}
